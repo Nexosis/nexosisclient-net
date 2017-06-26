85340a966bb38f9d6f6cffe6d520d8fb8c1213a2
Jeff Espenschied <jeffesp@nexosis.com>
2017-06-26 13:06:26 -0400

    Some doc comments to help explain things.

--- Api.Client/Model/SessionResponse.cs
+++ Api.Client/Model/SessionResponse.cs
@@ -7,32 +7,42 @@ namespace Nexosis.Api.Client.Model
 {
     public class SessionResponse : ReturnsCost
     {
+        /// <summary>The unique identifier of this session.</summary>
         public Guid SessionId { get; set; }
     
         [JsonConverter(typeof(StringEnumConverter))]
         public SessionType Type { get; set; }
     
+        /// <summary>The current <see cref="SessionStatus">status</see> of the session.</summary>
         [JsonConverter(typeof(StringEnumConverter))]
         public SessionStatus Status { get; set; }
 
+        /// <summary>The values of session status over time.</summary>
         public List<StatusChange> StatusHistory { get; set; } = new List<StatusChange>();
     
         public Dictionary<string, string> ExtraParameters { get; set; }
     
+        /// <summary>The name of the saved data set that has the data to forecast on.</summary>
         public string DataSetName { get; set; }
     
+        /// <summary>The name of the column for which you want predictions.</summary>
         public string TargetColumn { get; set; }
     
         public string EventName { get; set; }
 
+        /// <summary>The date and time the request for the session was initiated.</summary>
         public DateTimeOffset RequestedDate { get; set; }
 
+        /// <summary>The ending date of the session results.</summary>
         public DateTimeOffset StartDate { get; set; }
     
+        /// <summary>The ending date of the session results.</summary>
         public DateTimeOffset EndDate { get; set; }
 
+        /// <summary>Metadata about each column in the dataset</summary>
+        /// <remarks>Initialized with a case-insensitive key comparer as the API ignores case on column names.</remarks>
         public Dictionary<string, ColumnMetadata> Columns { get; set; } = new Dictionary<string, ColumnMetadata>(StringComparer.OrdinalIgnoreCase);
-
+        
         public List<Link> Links { get; set; }
     }
 
