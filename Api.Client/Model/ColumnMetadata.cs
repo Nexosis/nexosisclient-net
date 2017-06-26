5281aa8964db53ed93b32b9a9d79539a86a4d474
Craig Vermeer <craig@vermeers.net>
2017-06-26 16:37:49 -0400

    Make column role nullable

--- Api.Client/Model/ColumnMetadata.cs
+++ Api.Client/Model/ColumnMetadata.cs
@@ -3,7 +3,7 @@
     public class ColumnMetadata
     {
         public ColumnType DataType { get; set; }
-        public ColumnRole Role { get; set; }
+        public ColumnRole? Role { get; set; }
     }
 
     public enum ColumnType
