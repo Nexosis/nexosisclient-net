6057014022b42ca81860248525a6177604c52393
Craig Vermeer <craig@vermeers.net>
2017-12-11 10:54:22 -0500

    Import from url

---
+++ Api.Client.Tests/ImportTests/UrlImportTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Nexosis.Api.Client.Model;
+using Xunit;
+
+namespace Api.Client.Tests.ImportTests
+{
+    public class UrlImportTests : NexosisClient_TestsBase
+    {
+
+        public UrlImportTests() : base(new { }) { }
+
+        [Fact]
+        public async Task PostsToCorrectEndpoint()
+        {
+            var response = await target.Imports.ImportFromUrl(new ImportFromUrlRequest()
+            {
+                DataSetName = "my-data-set",
+                Url = "foo"
+            });
+
+            Assert.Equal(HttpMethod.Post, handler.Request.Method);
+            Assert.Equal(new Uri(@"https://nada.nexosis.com/imports/url"), this.handler.Request.RequestUri);
+        }
+
+        [Fact]
+        public async Task PassesCorrectParametersInBody()
+        {
+            var expectedResponse = new
+            {
+                Url="foo",
+                Auth = new
+                {
+                    Basic = new
+                    {
+                        UserId = "user",
+                        Password = "password"
+                    }
+                },
+                DataSetName = "my-data-set",
+                Columns = new Dictionary<string, ColumnMetadata>()
+                {
+                    {"foo", new ColumnMetadata() {DataType = ColumnType.Numeric}}
+                },
+                ContentType = ImportContentType.Json,
+            };
+
+            var response =
+                await target.Imports.ImportFromUrl(new ImportFromUrlRequest()
+                {
+                    Url = "foo",
+                    Auth = new ImportFromUrlAuthentication()
+                    {
+                        Basic = new BasicAuthentication()
+                        {
+                            UserId = "user",
+                            Password = "password"
+                        }
+                    },
+                    DataSetName = "my-data-set",
+                    ContentType = ImportContentType.Json,
+                    Columns = new Dictionary<string, ColumnMetadata>()
+                    {
+                        {"foo", new ColumnMetadata() {DataType = ColumnType.Numeric}}
+                    }
+                });
+
+            Assert.Equal(JsonConvert.SerializeObject(expectedResponse), this.handler.RequestBody);
+        }
+    }
+}
---
+++ Api.Client/Model/ImportFromUrlRequest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Nexosis.Api.Client.Model
+{
+    /// <summary>
+    /// The parameters needed to import data into the Nexosis API from a URL
+    /// </summary>
+    public class ImportFromUrlRequest : ImportRequest
+    {
+        /// <summary>
+        /// The URL to retrieve
+        /// </summary>
+        public string Url { get; set; }
+
+
+        /// <summary>
+        /// Authentication parameters to be used to retrieve the data at the given url
+        /// </summary>
+        public ImportFromUrlAuthentication Auth { get; set; }
+    }
+
+    /// <summary>
+    /// Url Authentication parameters
+    /// </summary>
+    public class ImportFromUrlAuthentication
+    {
+        /// <summary>
+        /// Basic Authentication
+        /// </summary>
+        public BasicAuthentication Basic { get; set; }
+    }
+
+    public class BasicAuthentication
+    {
+        /// <summary>
+        /// If the url is secured by basic authentication, use this username to authenticate
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// If the url is secured by basic authentication, use this password to authenticate
+        /// </summary>
+        public string Password { get; set; }
+    }
+
+
+}
