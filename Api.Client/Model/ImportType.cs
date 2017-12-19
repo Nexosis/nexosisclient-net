d1c7df8bf7ec78f0e19339558a378bab8cc75db2
Craig Vermeer <craig@vermeers.net>
2017-12-19 12:03:46 -0500

    Add Url and Azure to ImportType enum

--- Api.Client/Model/ImportType.cs
+++ Api.Client/Model/ImportType.cs
@@ -5,5 +5,7 @@ namespace Nexosis.Api.Client.Model
     public enum ImportType
     {
         [EnumMember(Value = "s3")] S3 = 0,
+        [EnumMember(Value = "url")] Url = 1,
+        [EnumMember(Value = "azure")] Azure = 2,
     }
 }
